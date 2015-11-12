0a9d320c25edabfa22d4b08f58d7a3ab40e9921e
Laurence Bortfeld <cheese@byteburgers.com>
2015-11-12 14:58:19 +0100

    Load JSON blob and js configuration file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  config: require(path.join(process.cwd(), './config/', (process.env.NODE_ENV || 'development') + '.json')),
+  config: require(path.join(process.cwd(), './config/', (process.env.NODE_ENV || 'development'))),
   isDebug: ! process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? false : true,
 };
 
