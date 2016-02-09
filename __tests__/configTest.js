bd15a34faac2caf872aaaf5b37322b7ebc60ded6
Laurence Bortfeld <cheese@byteburgers.com>
2016-02-09 07:53:36 +0100

    Fix const issues for node < v4

--- __tests__/configTest.js
+++ __tests__/configTest.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const chai = require('chai');
-const mocha = require('mocha');
+var chai = require('chai');
+var mocha = require('mocha');
 
-const after = mocha.after;
-const describe = mocha.describe;
-const it = mocha.it;
-const before = mocha.before;
-const expect = chai.expect;
+var after = mocha.after;
+var describe = mocha.describe;
+var it = mocha.it;
+var before = mocha.before;
+var expect = chai.expect;
 
 function clearRequireCache() {
   delete require.cache[require.resolve('../')];
@@ -15,13 +15,13 @@ function clearRequireCache() {
 
 describe('check no NODE_ENV set', function() {
   it('isDebug and __DEV__ musst be true in no environment', function() {
-    const debug = require('../').isDebug;
-    const __DEV__ = require('../').__DEV__;
+    var debug = require('../').isDebug;
+    var __DEV__ = require('../').__DEV__;
     expect(debug && __DEV__).to.equal(true);
   });
 
   it('Configuration should be the development config file', function() {
-    const config = require('../').config;
+    var config = require('../').config;
     expect(config.StarWars.movies[0]).to.equal('Star Wars');
   });
 
@@ -36,13 +36,13 @@ describe('Check development mode NODE_ENV !== "production"', function() {
   after(clearRequireCache);
 
   it('isDebug and __DEV__ musst be true in dev environment', function() {
-    const debug = require('../').isDebug;
-    const __DEV__ = require('../').__DEV__;
+    var debug = require('../').isDebug;
+    var __DEV__ = require('../').__DEV__;
     expect(debug && __DEV__).to.equal(true);
   });
 
   it('Configuration should be the development config file', function() {
-    const config = require('../').config;
+    var config = require('../').config;
     expect(config.StarWars.movies[6]).to.equal('The Force Awakens');
   });
 });
@@ -56,14 +56,14 @@ describe('Check production mode NODE_ENV === "production"', function() {
 
   it('isDebug and __DEV__ musst be false in production environment',
     function() {
-      const debug = require('../').isDebug;
-      const __DEV__ = require('../').__DEV__;
+      var debug = require('../').isDebug;
+      var __DEV__ = require('../').__DEV__;
       expect(debug || __DEV__).to.equal(false);
     }
   );
 
   it('Configuration should be the production config file', function() {
-    const config = require('../').config;
+    var config = require('../').config;
     expect(config.StarWars.characters[0]).to.equal('Han Solo');
   });
 });
@@ -76,13 +76,13 @@ describe('Check for NODE_ENV === "test"', function() {
   after(clearRequireCache);
 
   it('isDebug and __DEV__ musst be true in test environment', function() {
-    const debug = require('../').isDebug;
-    const __DEV__ = require('../').__DEV__;
+    var debug = require('../').isDebug;
+    var __DEV__ = require('../').__DEV__;
     expect(debug && __DEV__).to.equal(true);
   });
 
   it('Configuration should be the test config file', function() {
-    const config = require('../').config;
+    var config = require('../').config;
     expect(config.StarWars.planets[2]).to.equal('Jakku');
   });
 });
